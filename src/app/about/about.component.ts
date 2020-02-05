ab896b885cda733ef921029280fe5b30eedf8ea8
Bruno Conde Kind <brunocondekind@hotmail.com>
2020-02-04 22:14:58 -0200

    about component updated

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,18 +1,22 @@
-import { Component, AfterContentInit, Input } from '@angular/core'
+import { Component } from '@angular/core'
 import teamref from '../../assets/info/teamref.json'
+import { MatIconModule } from '@angular/material/icon'
+
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faAddressCard } from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent implements AfterContentInit {
+export class AboutComponent {
 
   members
+  faGithub = faGithub
+  faAddressCard = faAddressCard
 
   constructor() { this.members = teamref.data }
 
-  ngAfterContentInit() {
-  }
-
 }
+
--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -1,21 +1,24 @@
-import { NgModule } from '@angular/core'
-import { CommonModule } from '@angular/common'
+import { NgModule }           from '@angular/core'
+import { CommonModule }       from '@angular/common'
 
 import { AboutRoutingModule } from './about-routing.module'
-import { AboutComponent } from './about.component'
+import { AboutComponent }     from './about.component'
 
-import { MatSidenavModule } from '@angular/material/sidenav'
-import { MatCardModule } from '@angular/material/card'
-import { MatIconModule } from '@angular/material/icon'
+import { MatSidenavModule }   from '@angular/material/sidenav'
+import { MatCardModule }      from '@angular/material/card'
+import { MatIconModule }      from '@angular/material/icon'
+import { FontAwesomeModule }  from '@fortawesome/angular-fontawesome'
 
 @NgModule({
   declarations: [AboutComponent],
   imports: [
-    CommonModule,
-    AboutRoutingModule,
-    MatSidenavModule,
-    MatCardModule,
-    MatIconModule,
+  CommonModule,
+  AboutRoutingModule,
+  MatSidenavModule,
+  MatCardModule,
+  MatIconModule,
+  FontAwesomeModule,
   ]
 })
 export class AboutModule { }
+
